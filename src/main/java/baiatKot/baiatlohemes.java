c444ddc8fce5652e28c6a9b47f1a26e2aca2d471
Amal <uwgating@gmail.com>
2025-01-10 19:32:58 +0600

    new loh

--- src/main/java/baiatKot/baiatlohemes.java
+++ src/main/java/baiatKot/baiatlohemes.java
@@ -2,6 +2,6 @@ package baiatKot;
 
 public class baiatlohemes {
     public static void main(String[] args) {
-        System.out.println("lohemes");
+        System.out.println("lohokaz");
     }
 }
