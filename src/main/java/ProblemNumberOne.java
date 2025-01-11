ecc4400859805922484abb60146889ce19b0658a
Mona Lisa <makeshovqa@gmail.com>
2025-01-11 16:52:50 +0600

    problemOne

---
+++ src/main/java/ProblemNumberOne.java
@@ -0,0 +1,31 @@
+public class ProblemNumberOne {
+}
+
+
+/*
+aight so here today ive added some practice problems for u to solve,
+this is one of the easiest problems that was asked in my previous interviews,
+first goes the description then the details of the problem then after the constraints,
+also ull learn what the anagram is or other systems, methodologies, words,
+remember they will be asked in the interviews
+
+    HERE WE GO
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+
+Output: true
+
+Example 2:
+
+Input: s = "rat", t = "car"
+
+Output: false
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+ */
--- src/main/java/baiatKot/test.java
+++
@@ -1,7 +0,0 @@
-package baiatKot;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println("sex");
-    }
-}
